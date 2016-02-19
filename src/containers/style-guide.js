593e50c7e6189e27bf021581aae170cf143c2e61
sqrtofsaturn <sqrt@octoblu.com>
2016-02-18 18:22:02 -0700

    Not quite there yet. Still needs to import components from npm

--- src/containers/style-guide.js
+++ src/containers/style-guide.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import zooids from 'zooids'
 
 export default class StyleGuide extends Component {
   render() {
+    console.log(zooids);
     return <div>
-      <h1>Zooids</h1>
-      <h1>Hello, world</h1>
+      <h1>Zooids, The Style Guide</h1>
+      <h2>App Bar</h2>
+      <AppBar />
     </div>
   }
 }
